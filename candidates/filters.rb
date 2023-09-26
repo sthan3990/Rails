7cec136b37d1e125086f45bd4f209ea68369cc8b
phankons <stephen.han@gmx.com>
2023-09-26 13:23:35 -0400

    Create candidate filter functions

--- candidates/filters.rb
+++ candidates/filters.rb
@@ -2,7 +2,7 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-  # Your code Here
+  return @candidates.select{|x| x[:id]==id}
 end
 
 def experienced?(candidate)
@@ -15,10 +15,18 @@ def experienced?(candidate)
 end
 
 def qualified_candidates(candidates)
-  # Your code Here
+  qualified = []
+  @candidates.each do |x| 
+    if more_than_100_points?(x)&&ruby_or_python?(x)&&applied_15_days?(x)&&older_than_17?(x)
+      qualified << x
+    end
+  end
+  return qualified
 end
 
 def ordered_by_qualifications(candidates)
+  sorted = @candidates.sort {|a, b| [b[:years_of_experience], b[:github_points]] <=> [a[:years_of_experience], a[:years_of_experience]]}
+  return sorted
 end
 
 # More methods will go below
